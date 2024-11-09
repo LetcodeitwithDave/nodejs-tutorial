91cbe90caf983193bc3c6f590ccdb3658a3cb75e
Dave <sololadavid4@gmail.com>
2024-11-09 23:26:38 +0100

    learnt how to setup server

--- create-server.js
+++ create-server.js
@@ -5,7 +5,10 @@ const http = require('http');
 // createserver takse 2 argument ( request and response)
 
 const server = http.createServer((req,res) => {
+
+   
     console.log(req)
+    // process.exit()
 });
 
 
