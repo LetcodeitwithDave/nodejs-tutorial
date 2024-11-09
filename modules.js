f4bcd82db4ffbad957e03f9dab04558f43c7c0e8
Dave <sololadavid4@gmail.com>
2024-11-09 23:28:56 +0100

    inbuilt core module in nodejs

--- modules.js
+++ modules.js
@@ -1,5 +1,12 @@
 //CommonJS, every file is a module (by default)
 
+// Core modules 
+//  #https -> Launch a server, send request
+//  #https -> Launch a SSl server
+//  #fs
+//  #path
+//  #os
+
 const names =  require('./4-name')
 const sayHi =  require('./5-utils')
 console.log(names)
