2d80e8e9ab3e7001a4720c9039633341dd59a17c
Dave <sololadavid4@gmail.com>
2024-11-10 01:20:06 +0100

    learnt about routing request and passing response data

--- request-response.js
+++ request-response.js
@@ -4,13 +4,27 @@ const http = require('http');
 // createserver takse 2 argument ( request and response)
 
 const server = http.createServer((req,res) => {
+    const url = req.url;
+    
+    if( url === '/'){
+    res.setHeader('Content-Type', 'text-html');
+    res.write('<html>');
+    res.write('<head><title> Home page</title></head>');
+    res.write('<body> Hello from my node.js server! on home</body>');
+    res.write('</html>');
+    return res.end();
+    }
 
-    // understanding request
-    // console.log(req.url, req.method, req.headers )
-    // process.exit()
+    res.setHeader('Content-Type', 'text-html');
+    res.write('<html>');
+    res.write('<head><title>Random path</title></head>');
+    res.write('<body> unknown page</body>');
+    res.write('</html>');
+    res.end();
+    
 
-    console.log(res)
+    // console.log(res)
 });
 
 
-server.listen(3000)
+server.listen(3000) 
